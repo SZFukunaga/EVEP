5cfb3a6a161f627438f75138c2e108eb474b083a
Shuzo Fukunaga <shuzo@SZacFirst.local>
2022-11-16 09:30:40 +0900

    Modified.

--- R/randomData.R
+++ R/randomData.R
@@ -1,21 +1,60 @@
-set.seed(1004805945)
-
-#
-defaultRandomValue <- function(x, max, min, actual_value){
-  non_augmented_noise <-
-    runif(1, min = -1, max = 1) * actual_value * ((max - x) / (max - min))
-  augmented_noise <-
-    runif(1, min = -1, max = 1) * actual_value * (((max - x) / (max - min)) / 2)
-  non_augmented_value <- actual_value + non_augmented_noise
-  augmented_value <- actual_value + augmented_noise
-  return(c(non_augmented_value, augmented_value))
-}
+#' Generate Random Sample Dataset
+#'
+#' A function to generate random sample training data.
+#'
+#' @param smallest A integer that shows the smallest training data size. Default
+#' is set 100.
+#'
+#' @param largest A integer that shoes the largest training data size. Default
+#' is set 1000.
+#'
+#' @param num_data A size of the generated sample data. Default is set 1000.
+#'
+#' @param step A step of training data size between the parameters smallest and
+#' largest. Default is set 50.
+#'
+#' @return Returns a dataframe with four columns. The first column is the
+#' training data size, second and third are predicted values for non-augmented
+#' and augmented model. The last column is an actual value.
+#'
+#' @examples
+#' # Example 1:
+#' # Produce a sample random dataset and save as csv file.
+#'
+#' df_random_data <- generateRandomData()
+#' write.csv(df_random_data, "./data/sample_random_data.csv", row.names = FALSE)
+#'
+#' @references
+#'
+#' @export
+#' @import
 
 generateRandomData <- function(smallest=100,
                                largest=1000,
                                num_data=1000,
-                               step=50,
-                               fnc_value = defaultRandomValue){
+                               step=50){
+
+  # Set seed.
+  set.seed(1004805945)
+
+  # A helper function to produce random value.
+  defaultRandomValue <- function(x, max, min, actual_value){
+    # Generate a random noise. Minimum is -1 * actual_value and maximum is 1 *
+    # actual_value. The more training data size it has, the less noise it has.
+    non_augmented_noise <-
+      runif(1, min = -1, max = 1) * actual_value * ((max - x) / (max - min))
+    # Generate a random noise for augmented data. Expected value is a half of
+    # the one for non-augmented data, as it is expected that augmentation
+    # generally contributes to the improvement of VEP accuracy.
+    augmented_noise <-
+      runif(1, min = -1, max = 1) * actual_value * (((max - x) / (max - min)) / 2)
+    # Apply noises.
+    non_augmented_value <- actual_value + non_augmented_noise
+    augmented_value <- actual_value + augmented_noise
+    return(c(non_augmented_value, augmented_value))
+  }
+
+  # Initialize data frame.
   df_random_data = data.frame(data_size = 0,
                               non_augmented_value = 0,
                               augmented_value = 0,
@@ -31,9 +70,9 @@ generateRandomData <- function(smallest=100,
                      actual_value = actual_value)
           )
   }
+  # Remove the first row which is produced in initialization.
   df_random_data <- df_random_data[-1, ]
   return(df_random_data)
 }
 
-df_random_data <- generateRandomData()
-write.csv(df_random_data, "./data/sample_random_data.csv", row.names = FALSE)
+# [END]
---
+++ R/visualizeEvaluation.R
@@ -0,0 +1,68 @@
+#' Visualize VEP Evaluation
+#'
+#' A function to visualize the relationships between accuracy of VEP and
+#' training data size, or and data augmentation.
+#'
+#' @param infile A path to a training data to be evaluated.
+#'
+#' @param stats A statistical measure to calculate. This has to be either
+#' "pearson", "kenall" or "mse". "pearson" is set as a default.
+#'
+#' @param label A label for the statistical measure. Used as a column name of
+#' returned dataframe.
+#'
+#' @param fnc_stats A function to conduct user-defined statistical analysis. The
+#' first argument should be a dataframe to analyse, with their data size the
+#' same. The second one is method, which does not have to be involved, just for
+#' consistency. The thrid argument should be the data size given in the first
+#' argument, and the last argument is label, which is the same as the parameter
+#' above. If the parameter stats is given, fnc_stats should be NA.
+#'
+#' @return Returns NA, as the graph is shown by this function.
+#'
+#' @examples
+#' # Example 1:
+#' # Visualize the evaluation of VEP with pearson correlation.
+#' # sample_random_data.csv is used as a training dataset.
+#'
+#' visualizeEvaluation("./data/sample_random_data.csv")
+#'
+#' # Example 2:
+#' Visualize the evaluation of VEP with mean squared error.
+#' # sample_random_data.csv is used as a training dataset.
+#'
+#' visualizeEvaluation("./data/sample_random_data.csv",
+#'                       stats = "mse",
+#'                       label = "mean_squared_error",
+#'                       fnc_stats = NA)
+#'
+#' @references
+#'
+#' @export
+#' @import
+
+visualizeEvaluation <- function(infile,
+                                stats = "pearson",
+                                label = "pearson correlation",
+                                fnc_stats = NA){
+
+  # Obtain statistical measures with evaluateVEP().
+  df_evaluation <- evaluateVEP(infile, stats, label, fnc_stats)
+  # Plot non-augmented VEP.
+  plot(df_evaluation$data_size,
+       df_evaluation[, 2],
+       xlab = "training data size",
+       ylab = label,
+       pch = 19)
+  # Plot augmented VEP.
+  points(df_evaluation$data_size, df_evaluation[, 3], col="red", pch = 19)
+  # Add legend and title.
+  legend("bottomright",
+         legend = c("Not Augmented", "Augmented"),
+         col = c("black", "red"),
+         pch = 19)
+  title("Evaluation of Agumentation on VEP")
+  return(NA)
+}
+
+# [END]
